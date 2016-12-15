3c6bef381e8e3a8f7e396d9287a06c7707d4e92c
zakattack9 <zakattack26111@gmail.com>
2016-12-15 09:36:31 -1000

    JAVASCRIPT HTML CSS

---
+++ JS/app.js
@@ -0,0 +1,22 @@
+console.log("JavaScript is Wokring")
+
+/*function red(){
+  var z = document.getElementById("box");
+  z.id = "redbox";
+}
+function green(){
+  var y = document.getElementById("redbox");
+  y.id = "box";
+}*/
+
+function change(){
+  var x = document.getElementsByClassName("box2");
+  x.id = "redbox";
+  /*if(x.style.backgroundColor = blue && x.style.border = 5px purple dashed){
+    x.style.backgroundColor = purple && x.style.border = 5px blue solid;
+  }
+  else if(x.style.backgroundColor = purple && x.style.border = 5px blue solid){
+    x.style.backgroundColor = blue && x.style.border = 5px purple dashed;
+  }*/
+}
+
