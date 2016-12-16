492dd14b9a53bce2ea56643184b058ceff18a4ed
zakattack9 <zakattack26111@gmail.com>
2016-12-16 08:19:28 -1000

    somewhat works

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,13 @@
 console.log("JavaScript is Wokring")
 
-/*function red(){
+function red(){
   var z = document.getElementById("box");
   z.id = "redbox";
 }
 function green(){
   var y = document.getElementById("redbox");
   y.id = "box";
-}*/
+}
 
 function change(){
   var x = document.getElementsByClassName("box2");
@@ -18,5 +18,7 @@ function change(){
   else if(x.style.backgroundColor = purple && x.style.border = 5px blue solid){
     x.style.backgroundColor = blue && x.style.border = 5px purple dashed;
   }*/
+
+  //the code above doesn't work, when it is running, it causes the red and green function to stop working also
 }
 
